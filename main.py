9d4f6fc4a0c879428cb5801d6c5ed8fbe062ab0f
joaopulsz <joaopulsz@hotmail.com>
2023-02-13 08:57:34 -0300

    added paddles and movement functionatility

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from turtle import Screen
+from paddle import Paddle
+import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=800)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
+
+left_paddle = Paddle(-360)
+right_paddle = Paddle(360)
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
+
+
+playing = True
+while playing:
+    screen.update()
+    time.sleep(0.1)
 
 
 
---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, x_coordinate):
+        super().__init__()
+        self.shape("square")
+        self.turtlesize(stretch_wid=8, stretch_len=-1)
+        self.color("white")
+        self.penup()
+        self.goto(x_coordinate, 0)
+
+    def up(self):
+        y_coordinate = self.ycor()
+        self.goto(self.xcor(), y_coordinate + 25)
+
+    def down(self):
+        y_coordinate = self.ycor()
+        self.goto(self.xcor(), y_coordinate - 25)
